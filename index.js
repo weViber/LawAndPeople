a1ebe2edc75fbe0a884ca058ebc0c15bf3c5f502
Kun Woo Kim <kimkuns98@gmail.com>
2024-04-12 13:07:33 +0900

    fixed

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
-// @ts-check
+// // @ts-check
 
-require('dotenv').config()
+// require('dotenv').config()
 
-const app = require('./src/app')
+// const app = require('./src/app')
 
-const { PORT } = require('./src/common')
+// const { PORT } = require('./src/common')
+// const mongo = require('./src/mongo')
+
+// mongo
+//     .connectToDatabase()
+//     .then(() => {
+//         app.listen(PORT, () => {
+//             console.log(`App listening at http://localhost:${PORT}`)
+//         })
+//     })
+//     .catch((error) => {
+//         console.error('Failed to connect to MongoDB:', error)
+//         throw error
+//     })
+
+// index.js
+const express = require('express')
+const cors = require("cors");
+const path = require('path');
 const mongo = require('./src/mongo')
+
+const app = express()
+const PORT = 4000
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const apiRouter = require('./src/routes/index');
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 mongo
     .connectToDatabase()
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`App listening at http://localhost:${PORT}`)
+            console.log(`API listening on PORT ${PORT} `)
         })
+
     })
     .catch((error) => {
         console.error('Failed to connect to MongoDB:', error)
         throw error
     })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use('/api', apiRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// Export the Express API
+module.exports = app
